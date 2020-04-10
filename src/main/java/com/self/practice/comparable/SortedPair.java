f7b7fd4f610b8daf145e4f85b38908c8bf8aa1c8
Bagesh Sharma <bagesh.sharma@orange.com>
2020-04-10 21:08:44 +0530

    Initial Commit

---
+++ src/main/java/com/self/practice/comparable/SortedPair.java
@@ -0,0 +1,25 @@
+package com.self.practice.comparable;
+
+public class SortedPair<T extends Comparable<T>> {
+
+	private final T first;
+	private final T second;
+
+	public SortedPair(T left, T right) {
+		if (left.compareTo(right) < 0) {
+			first = left;
+			second = right;
+		} else {
+			first = right;
+			second = left;
+		}
+	}
+
+	public T getFirst() {
+		return first;
+	}
+
+	public T getSecond() {
+		return second;
+	}
+}
---
+++ src/main/java/com/self/practice/comparable/SortedPairTest.java
@@ -0,0 +1,24 @@
+package com.self.practice.comparable;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SortedPairTest {
+
+	@Test
+	public void shouldRetainOrderOfOrderedPair() {
+		SortedPair<Integer> pair = new SortedPair<>(1, 2);
+
+		assertEquals(1, pair.getFirst().intValue());
+		assertEquals(2, pair.getSecond().intValue());
+	}
+
+	@Test
+	public void shouldFlipOrderOfMisOrderedPair() {
+		SortedPair<Integer> pair = new SortedPair<>(2, 1);
+
+		assertEquals(1, pair.getFirst().intValue());
+		assertEquals(2, pair.getSecond().intValue());
+	}
+}
